5d826648a0d6685597cf062220e93715e61499ab
Fabiano Júnior <50252807+fabianosantos23@users.noreply.github.com>
2022-11-17 20:48:11 -0300

    add tsx dev depencies

--- server/src/routes/products.ts
+++ server/src/routes/products.ts
@@ -2,6 +2,8 @@ import { FastifyInstance } from "fastify"
 
 export async function productRoutes(fastify: FastifyInstance) {
   fastify.get('/pools/count', async () => {
-
+    return {
+      "ok": "demais"
+    }
   })
 }
