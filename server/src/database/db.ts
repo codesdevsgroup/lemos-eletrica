c584ee36c975ac889451aa945062e88715c11fbd
Fabiano Júnior <50252807+fabianosantos23@users.noreply.github.com>
2022-11-19 19:15:44 -0300

    products route

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -1,357 +1,353 @@
-import {Sequelize, DataTypes} from "sequelize";
-
-const dbMysql = new Sequelize("u171877228_pegapreco", "u171877228_WB8Zd", "Codesd3v5FjL++", {
-    host: 'sql912.main-hosting.eu',
-    dialect: 'mysql'
-});
+import { DataType } from "sequelize-typescript";
+import { dbMysql } from "./connection";
 
 dbMysql.authenticate().then().catch(function(error){
   console.log(error);
 });
 
-const produtos = dbMysql.define('PRODUTOS', {
+const Produto = dbMysql.define('PRODUTOS', {
   PRODUTOS_ID: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: false,
     primaryKey: true
   },
   PRODUTO_TIPO: {
-    type: DataTypes.STRING,
+    type: DataType.STRING,
     allowNull: false,
   },
   PRODUTO_INATIVO: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: false,
   },
   PRODUTO_IAT: {
-    type: DataTypes.STRING,
+    type: DataType.STRING,
     allowNull: false,
   },
   PRODUTO_IPPT: {
-    type: DataTypes.STRING,
+    type: DataType.STRING,
     allowNull: false,
   },
   UNIDADES_ID: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: false,
   },
   PRODUTO_FRACAO_VENDA: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_NOME: {
-    type: DataTypes.STRING,
+    type: DataType.STRING,
     allowNull: false,
   },
   DEPARTAMENTOS_ID: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: false,
   },
   SUBGRUPOS_ID: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: false,
   },
   FAMILIAS_ID: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   MARCAS_ID: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   PRODUTO_PRECO_COMPRA: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_PRECO_CUSTO: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_MARGEM_LUCRO: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_PRECO_VENDA: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_PESO_BRUTO: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_PESO_LIQUIDO: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_PESO_VOLUME: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_FATOR_CONVERSAO: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_PRIORIDADE: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: false,
   },
   PRODUTO_ESTOQUE_MINIMO: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_ESTOQUE_DISPONIVEL: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   CSTIPI_ID: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: true,
   },
   PRODUTO_BCIPI_RED: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_IPI: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_EXTIPI: {
-    type: DataTypes.STRING,
+    type: DataType.STRING,
     allowNull: true,
   },
   PRODUTO_IMPOSTO: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   CSTPIS_ID: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: true,
   },
   PRODUTO_PIS: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_CSTPIS_ENTRADA: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: true,
   },
   PRODUTO_PIS_ENTRADA: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_NAT_REC_PIS: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   PRODUTO_COD_CRED_PIS: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   PRODUTO_COD_REC_PIS: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   }, 
   CSTCOFINS_ID: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: true,
   },
   PRODUTO_COFINS: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_CSTCOFINS_ENTRADA: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: true,
   },
   PRODUTO_COFINS_ENTRADA: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   PRODUTO_NAT_REC_COFINS: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   PRODUTO_COD_CRED_COFINS: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   PRODUTO_COD_REC_COFINS: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   NCM_ID: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: false,
   },
   PRODUTO_ANP: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   PRODUTO_BCICMS_MOD: {
-    type: DataTypes.STRING,
+    type: DataType.STRING,
     allowNull: false,
   },
   PRODUTO_APLICACAO: {
-    type: DataTypes.STRING,
+    type: DataType.STRING,
     allowNull: true,
   },
   PRODUTO_LOCALIZACAO: {
-    type: DataTypes.STRING,
+    type: DataType.STRING,
     allowNull: true,
   },
   INFORMACOES_ADICIONAIS: {
-    type: DataTypes.STRING,
+    type: DataType.STRING,
     allowNull: true,
   },
   PRODUTO_TAG: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: true,
   },
   PRODUTO_QUANTIDADE_ETIQUETAS: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   MODIFICADO: {
-    type: DataTypes.STRING,
+    type: DataType.STRING,
     allowNull: true,
   },
   BLOQUEADO: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   PRODUTO_IMPORTADO: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   PRODUTO_ESTOQUE_CONSIGNADO: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: true,
   },
   ENQ_IPI_ID: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   CEST_ID: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   PRODUTO_COD_BALANCA: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   PRODUTO_SYNC_FALCON: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: false,
   },
   PERCENTUAL_GLP: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   REGISTRO_ANVISA: {
-    type: DataTypes.STRING,
+    type: DataType.STRING,
     allowNull: true,
   },
   RASTRO: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: false,
   },
   FABRICANTES_ID: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   PROD_ESCALA_NAO_RELEVANTE: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: false,
   },
   BENEFICIO_ESCALA_N_RELEVANTE: {
-    type: DataTypes.STRING,
+    type: DataType.STRING,
     allowNull: true,
   },
   PERCENTUAL_GNN: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: true,
   },
   PERCENTUAL_GNI: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: true,
   },
   VALOR_PARTIDA_GLP: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: true,
   },
   PRODUTO_CRED_CIAP: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: false,
   },
   TIPO_COMERCIAL: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: false,
   },
   UNIDADE_TRIBUTARIA_ID: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: true,
   },
   FATOR_CONVERSAO_VENDA: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   SOLICIT_FATOR_CONVERSAO_TRIB: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: false,
   },
   CODIGO_FCI: {
-    type: DataTypes.STRING,
+    type: DataType.STRING,
     allowNull: true,
   },
   GERAR_FCI: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: false,
   },
   VALOR_PARCELA_IMPORTADA: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: true,
   },
   CONTEUDO_IMPORTACAO_FCI: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: true,
   },
   VALOR_CMV: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: false,
   },
   DECOMPOR_AUTO_ENTRADA: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: false,
   },
   COMBUSTIVEL_OU_LUBRIFICANTE: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: false,
   },
   PRODUTO_NAT_REC_PIS_ID: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   PRODUTO_NAT_REC_COFINS_ID: {
-    type: DataTypes.INTEGER,
+    type: DataType.INTEGER,
     allowNull: true,
   },
   CUBAGEM: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: true,
   },
   ADD_COMPONENTES_INDIVIDUALMENTE: {
-    type: DataTypes.TINYINT,
+    type: DataType.TINYINT,
     allowNull: false,
   },
   VALOR_PMC_ANVISA: {
-    type: DataTypes.FLOAT,
+    type: DataType.FLOAT,
     allowNull: true,
   },
   MOTIVO_ISENCAO_ANVISA: {
-    type: DataTypes.STRING,
+    type: DataType.STRING,
     allowNull: true,
   }, 
 });
 
-produtos.sync({ alter: true })
-export {produtos}
+Produto.sync({ alter: true })
+export {Produto}
--- server/src/routes/products.ts
+++ server/src/routes/products.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from "fastify"
+import { Produto } from "../database/db";
 
 export async function productRoutes(fastify: FastifyInstance) {
   fastify.get('/products', async () => {
-    return {
-      "ok": "demais"
-    }
+    const produtos = await Produto.findAll({
+      attributes: ['PRODUTOS_ID', 'PRODUTO_NOME', 'PRODUTO_PRECO_VENDA']
+    });
+
+    return produtos
   })
 }
